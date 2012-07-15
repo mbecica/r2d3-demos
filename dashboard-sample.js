8b5b49b2a9ff0b257b0eb0e4a5c76813522fc738
Mary Becica <mary.becica@appdirect.com>
2012-07-15 15:32:38 -0700

    stacked bar example

---
+++ dashboard-sample.js
@@ -0,0 +1,146 @@
+// Revenue Line Chart
+var revenue = [
+    // One list of total
+	[{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	}],
+    // One list of gross
+    [{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	}]
+]
+
+// Users Line Chart
+var users = [
+	{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	},
+]
+
+// Orders bar chart
+var orders = [
+	//One list of free
+	[{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	}],
+	//One list of paid
+	[{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	}],
+	//One list of totals
+	[{
+		x: 1333065600000, // Date
+		y: 23,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 20,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 16,010 // Value
+	},
+	{
+		x: 1333065600000, // Date
+		y: 12,000 // Value
+	}]
+]
+
+// Top Apps, limit 5
+var topApps = [
+	{
+		name: "App Name",
+		revenue: 29,199,
+		tags: ["tagName", "tagName", "tagName"]
+	},
+	{
+		name: "App Name",
+		revenue: 29,199,
+		tags: ["tagName", "tagName", "tagName"]
+	},
+	{
+		name: "App Name",
+		revenue: 29,199,
+		tags: ["tagName", "tagName", "tagName"]
+	},
+	{
+		name: "App Name",
+		revenue: 29,199,
+		tags: ["tagName", "tagName", "tagName"]
+	},
+	{
+		name: "App Name",
+		revenue: 29,199,
+		tags: ["tagName", "tagName", "tagName"]
+	},
+	// The rest of the apps total
+	{
+		name: "Other",
+		revenue: 29,199
+	}
+]
